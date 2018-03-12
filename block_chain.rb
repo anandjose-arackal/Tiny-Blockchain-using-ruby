e26a2eafd80032a9205203b47e4aa652630960c1
Anand Jose <anand.jose@trivand.com>
2018-03-12 19:47:40 +0530

    Intial commit

---
+++ block_chain.rb
@@ -0,0 +1,51 @@
+require "digest"    # for hash checksum digest function SHA256
+require "pp"        # for pp => pretty printer
+
+
+class Block
+
+  attr_reader :index
+  attr_reader :timestamp
+  attr_reader :data
+  attr_reader :previous_hash
+  attr_reader :hash
+
+  def initialize(index, data, previous_hash)
+    @index         = index
+    @timestamp     = Time.now
+    @data          = data
+    @previous_hash = previous_hash
+    @hash          = calc_hash
+  end
+
+  def calc_hash
+    sha = Digest::SHA256.new
+    sha.update( @index.to_s + @timestamp.to_s + @data + @previous_hash )
+    sha.hexdigest
+  end
+
+
+  def self.first( data="Genesis" )    # create genesis (big bang! first) block
+    ## uses index zero (0) and arbitrary previous_hash ("0")
+    Block.new( 0, data, "0" )
+  end
+
+  def self.next( previous, data="Transaction Data..." )
+    Block.new( previous.index+1, data, previous.hash )
+  end
+
+end  # class Block
+
+
+#####
+## let's get started
+##   build a blockchain a block at a time
+
+b0 = Block.first( "Genesis" )
+b1 = Block.next( b0, "Transaction Data..." )
+b2 = Block.next( b1, "Transaction Data......" )
+b3 = Block.next( b2, "More Transaction Data..." )
+
+blockchain = [b0, b1, b2, b3]
+
+pp blockchain
